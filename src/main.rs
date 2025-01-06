5344139bec032d1ec5626a52dda19546264e282d
Eduardo Flores <edfloreshz@gmail.com>
2025-01-06 01:55:59 +0100

    chore: update core

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,4 @@
-use bottles_core::proto::{
-    self,
-    wine_bridge_server::{WineBridge, WineBridgeServer},
-};
-use windows::{
-    core::{s, PCSTR},
-    Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK},
-};
-
-#[derive(Debug, Default)]
-struct WineBridgeService;
-
-#[tonic::async_trait]
-impl WineBridge for WineBridgeService {
-    async fn message(
-        &self,
-        request: tonic::Request<proto::MessageRequest>,
-    ) -> Result<tonic::Response<proto::MessageResponse>, tonic::Status> {
-        let request = request.get_ref();
-        println!("Got a request: {:?}", request);
-        let message = request.message.as_str();
-        let c_message = std::ffi::CString::new(message).unwrap();
-        unsafe {
-            MessageBoxA(
-                None,
-                PCSTR(c_message.as_ptr() as *const u8),
-                s!("Hello"),
-                MB_OK,
-            );
-        }
-        Ok(tonic::Response::new(proto::MessageResponse {
-            success: true,
-        }))
-    }
-}
+use bottles_core::{proto::wine_bridge_server::WineBridgeServer, winebridge::WineBridgeService};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
