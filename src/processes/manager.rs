a48edb0ca63452682b34bcfa5a1a970cbbc71921
Cyber Phantom <inam123451@gmail.com>
2025-01-14 23:26:47 +0530

    feat: Implement KillProcess

--- src/processes/manager.rs
+++ src/processes/manager.rs
@@ -1,4 +1,4 @@
-use super::process::{Process, ProcessSnapshot};
+use super::process::{Process, ProcessIdentifier, ProcessSnapshot};
 use windows::core::Error;
 
 struct ProcessManager;
@@ -8,5 +8,19 @@ impl ProcessManager {
         let snapshot = ProcessSnapshot::new()?;
 
         Ok(snapshot.map(|process| process).collect())
+    }
+
+    pub fn process(&self, identifier: ProcessIdentifier) -> Option<Process> {
+        let processes = self
+            .running_processes()
+            .expect("Failed to get running processes");
+
+        match identifier {
+            ProcessIdentifier::Name(name) => processes
+                .iter()
+                .find(|p| p.name().to_lowercase() == name.to_lowercase())
+                .cloned(),
+            ProcessIdentifier::PID(pid) => processes.iter().find(|p| p.pid() == pid).cloned(),
+        }
     }
 }
