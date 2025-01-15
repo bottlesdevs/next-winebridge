d37beb6bbce2a7a3623cca7931bcb35381253164
Cyber Phantom <inam123451@gmail.com>
2025-01-16 00:43:55 +0530

    feat: Implement RPC server calls

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod processes;
 
 use bottles_core::proto::{self, wine_bridge_server::WineBridge};
+use processes::{manager::ProcessManager, process::ProcessIdentifier};
 use windows::{
     core::{s, PCSTR},
     Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK},
@@ -30,5 +31,60 @@ impl WineBridge for WineBridgeService {
         Ok(tonic::Response::new(proto::MessageResponse {
             success: true,
         }))
+    }
+
+    async fn running_processes(
+        &self,
+        _request: tonic::Request<proto::RunningProcessesRequest>,
+    ) -> Result<tonic::Response<proto::RunningProcessesResponse>, tonic::Status> {
+        let processes = ProcessManager.running_processes().map_err(|e| {
+            tonic::Status::internal(format!("Failed to get running processes: {:?}", e))
+        })?;
+
+        let processes = processes
+            .iter()
+            .map(|process| proto::Process {
+                name: process.name(),
+                pid: process.pid(),
+                threads: process.thread_count(),
+            })
+            .collect();
+
+        Ok(tonic::Response::new(proto::RunningProcessesResponse {
+            processes,
+        }))
+    }
+
+    async fn create_process(
+        &self,
+        request: tonic::Request<proto::CreateProcessRequest>,
+    ) -> Result<tonic::Response<proto::CreateProcessResponse>, tonic::Status> {
+        let input = request.into_inner();
+        let program = std::path::Path::new(&input.command);
+        let args = input.args;
+
+        ProcessManager
+            .execute(program, args)
+            .map_err(|e| tonic::Status::internal(format!("Failed to execute process: {:?}", e)))?;
+
+        Ok(tonic::Response::new(proto::CreateProcessResponse {
+            pid: 0,
+        }))
+    }
+
+    async fn kill_process(
+        &self,
+        request: tonic::Request<proto::KillProcessRequest>,
+    ) -> Result<tonic::Response<proto::KillProcessResponse>, tonic::Status> {
+        let pid = request.get_ref().pid;
+        let process = ProcessManager
+            .process(ProcessIdentifier::PID(pid))
+            .ok_or_else(|| tonic::Status::not_found("Process not found"))?;
+
+        process
+            .kill()
+            .map_err(|e| tonic::Status::internal(format!("Failed to kill process: {:?}", e)))?;
+
+        Ok(tonic::Response::new(proto::KillProcessResponse {}))
     }
 }
