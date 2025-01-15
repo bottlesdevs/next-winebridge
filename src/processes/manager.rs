1a1761ca371f406c8c6e1ac8118f436fc1e96fd6
Cyber Phantom <inam123451@gmail.com>
2025-01-15 20:29:51 +0530

    feat: Implement process creation

--- src/processes/manager.rs
+++ src/processes/manager.rs
@@ -1,5 +1,13 @@
-use super::process::{Process, ProcessIdentifier, ProcessSnapshot};
-use windows::core::Error;
+use super::process::{Process, ProcessIdentifier, ProcessInfo, ProcessSnapshot};
+use std::{ffi::OsStr, os::windows::ffi::OsStrExt, path::Path};
+use windows::{
+    core::{Error, PCWSTR, PWSTR},
+    Win32::System::Threading::{CreateProcessW, CREATE_NEW_CONSOLE, STARTUPINFOW},
+};
+
+fn to_wide_string(s: impl AsRef<OsStr>) -> Vec<u16> {
+    s.as_ref().encode_wide().chain(Some(0)).collect()
+}
 
 struct ProcessManager;
 
@@ -21,6 +29,30 @@ impl ProcessManager {
                 .find(|p| p.name().to_lowercase() == name.to_lowercase())
                 .cloned(),
             ProcessIdentifier::PID(pid) => processes.iter().find(|p| p.pid() == pid).cloned(),
+        }
+    }
+
+    fn execute(&self, executable: &Path, args: Vec<String>) -> Result<(), Error> {
+        let mut executable = to_wide_string(executable);
+        let mut args = to_wide_string(args.join(" "));
+
+        let mut startup_info = STARTUPINFOW::default();
+        startup_info.cb = std::mem::size_of::<STARTUPINFOW>() as u32;
+        let mut process_info = ProcessInfo::default();
+
+        unsafe {
+            CreateProcessW(
+                PCWSTR(executable.as_mut_ptr()),
+                Some(PWSTR(args.as_mut_ptr())),
+                None,
+                None,
+                false,
+                CREATE_NEW_CONSOLE,
+                None,
+                PCWSTR::null(),
+                &mut startup_info,
+                &mut process_info.0,
+            )
         }
     }
 }
