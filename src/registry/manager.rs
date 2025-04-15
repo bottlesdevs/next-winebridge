f7f39558b087d288d2d15bec367167238c6be4ad
Cyber Phantom <inam123451@gmail.com>
2025-04-15 21:18:53 +0530

    registry: Implement Trait Extensions for Key

--- src/registry/manager.rs
+++ src/registry/manager.rs
@@ -1,13 +1,7 @@
+use std::path::Path;
 use windows_registry::*;
 
-pub enum Hive {
-    ClassesRoot,
-    CurrentConfig,
-    CurrentUser,
-    LocalMachine,
-    Users,
-}
-
+#[derive(Debug, Eq, PartialEq)]
 pub enum Data {
     DWord(u32),
     QWord(u64),
@@ -18,8 +12,17 @@ pub enum Data {
     Other,
 }
 
+#[derive(Debug, Copy, Clone)]
+pub enum Hive {
+    ClassesRoot,
+    CurrentConfig,
+    CurrentUser,
+    LocalMachine,
+    Users,
+}
+
 impl Hive {
-    pub fn key(&self) -> &Key {
+    pub fn inner(&self) -> &Key {
         match self {
             Hive::ClassesRoot => CLASSES_ROOT,
             Hive::CurrentConfig => CURRENT_CONFIG,
@@ -30,47 +33,71 @@ impl Hive {
     }
 }
 
+trait KeyExtension {
+    fn get(hive: Hive, subkey: &Path) -> Result<Key> {
+        hive.inner().open(subkey.display().to_string())
+    }
+
+    fn new(hive: Hive, subkey: &Path) -> Result<Key> {
+        hive.inner().create(subkey.display().to_string())
+    }
+
+    fn delete(hive: Hive, subkey: &Path) -> Result<()> {
+        hive.inner().remove_tree(subkey.display().to_string())
+    }
+
+    fn value(&self, name: &str) -> Result<Value>;
+    fn values(&self) -> Result<Vec<(String, Value)>>;
+    fn create_value(&self, name: &str, data: Data) -> Result<()>;
+    fn rename_value(&self, old_name: &str, new_name: &str) -> Result<()>;
+}
+
+impl KeyExtension for windows_registry::Key {
+    fn value(&self, name: &str) -> Result<Value> {
+        let value = self.get_value(name)?;
+        Ok(value)
+    }
+
+    fn values(&self) -> Result<Vec<(String, Value)>> {
+        let mut values = Vec::new();
+        for value in self.values()? {
+            values.push(value);
+        }
+        Ok(values)
+    }
+
+    fn create_value(&self, name: &str, data: Data) -> Result<()> {
+        match data {
+            Data::Bytes(val) => self.set_bytes(name, Type::Bytes, &val),
+            Data::DWord(val) => self.set_u32(name, val),
+            Data::QWord(val) => self.set_u64(name, val),
+            Data::String(val) => self.set_string(name, &val),
+            Data::ExpandString(val) => self.set_expand_string(name, &val),
+            Data::MultiString(val) => {
+                let d: Vec<&str> = val.iter().map(|s| s.as_str()).collect();
+                self.set_multi_string(name, &d)
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn rename_value(&self, old_name: &str, new_name: &str) -> Result<()> {
+        let value = self.get_value(old_name)?;
+        self.remove_value(old_name)?;
+        self.set_value(new_name, &value)
+    }
+}
+
 pub struct RegistryManager;
 
 impl RegistryManager {
     pub fn value(&self, hive: &Hive, subkey: &str, name: &str) -> Result<Value> {
-        let key = hive.key().open(subkey)?;
+        let key = hive.inner().open(subkey)?;
 
-        key.get_value(name)
-    }
-
-    pub fn set_value(&self, hive: &Hive, subkey: &str, name: &str, data: Data) -> Result<()> {
-        let key = hive.key().create(subkey)?;
-
-        match data {
-            Data::Bytes(val) => key.set_bytes(name, Type::Bytes, &val),
-            Data::DWord(val) => key.set_u32(name, val),
-            Data::QWord(val) => key.set_u64(name, val),
-            Data::String(val) => key.set_string(name, &val),
-            Data::ExpandString(val) => key.set_expand_string(name, &val),
-            Data::MultiString(val) => {
-                let d: Vec<&str> = val.iter().map(|s| s.as_str()).collect();
-                key.set_multi_string(name, &d)
-            }
-            _ => unimplemented!(),
-        }
-    }
-
-    pub fn delete_value(&self, hive: &Hive, subkey: &str, name: &str) -> Result<()> {
-        let key = hive.key().open(subkey)?;
-
-        key.remove_value(name)
+        key.value(name)
     }
 
     pub fn key(&self, hive: &Hive, subkey: &str) -> Result<Key> {
-        hive.key().open(subkey)
-    }
-
-    pub fn create_key(&self, hive: &Hive, subkey: &str) -> Result<Key> {
-        hive.key().create(subkey)
-    }
-
-    pub fn delete_key(&self, hive: &Hive, subkey: &str) -> Result<()> {
-        hive.key().remove_tree(subkey)
+        hive.inner().open(subkey)
     }
 }
