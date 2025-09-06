2448041dfe246483453b96a8c8a6d8f9609c9dfb
Eduardo Flores <edfloreshz@gmail.com>
2025-09-06 00:20:24 -0700

    add: tracing-subscriber

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ use bottles_winebridge::WineBridgeService;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     let addr = "[::1]:50051".parse().unwrap();
     let service = WineBridgeService::default();
-    println!("Listening on {}", addr);
+    tracing::info!("Listening on {}", addr);
     tonic::transport::Server::builder()
         .add_service(WineBridgeServer::new(service))
         .serve(addr)
