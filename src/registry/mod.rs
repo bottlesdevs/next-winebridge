56fde0feb4fb3c6c5ae4d28a2427191e35081d29
Inam Ul Haq <inam123451@gmail.com>
2026-07-05 01:53:10 +0530

    registry: impl TryFrom<i32> for Hive

--- src/registry/mod.rs
+++ src/registry/mod.rs
@@ -1,1 +1,12 @@
 pub mod manager;
+
+impl TryFrom<i32> for manager::Hive {
+    type Error = tonic::Status;
+
+    fn try_from(hive: i32) -> Result<Self, Self::Error> {
+        bottles_core::RegistryHive::try_from(hive)
+            .map_err(|_| tonic::Status::invalid_argument("invalid registry hive"))?
+            .try_into()
+            .map_err(tonic::Status::invalid_argument)
+    }
+}
